3e5b328eba96b6f7a7c8a2e9609c4c454054c20e
James-cloudcannon <james@cloudcannon.com>
2023-08-30 16:11:34 +1200

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,34 @@
+const pluginBookshop = require("@bookshop/eleventy-bookshop");
+const yaml = require("js-yaml");
+
+
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets/images");
+    eleventyConfig.addPassthroughCopy("src/assets/fonts");
+    eleventyConfig.addPassthroughCopy("src/assets/js");
+
+    // Data extensions
+    eleventyConfig.addDataExtension('yaml', contents => yaml.load(contents))
+	eleventyConfig.addDataExtension('yml', contents => yaml.load(contents))
+
+    // Bookshop
+    eleventyConfig.addWatchTarget("component-library/");
+
+    eleventyConfig.addPlugin(pluginBookshop({
+		bookshopLocations: ["component-library"],
+		pathPrefix: '',
+	}));
+
+
+
+    // Filters
+
+
+    return {
+        dir: {
+            input: "src",
+            output: "_site"
+        }
+    }
+}
