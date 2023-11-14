20911eba6e894c9b2101000fcc184e521ce31a46
Adon Moskal <adonmoskal@Adons-MBP.cloudcannon.office>
2023-11-15 09:07:34 +1300

    Fix: fixed gallery JS - pagination and button functionality appears to be working

--- src/assets/js/gallery.js
+++ src/assets/js/gallery.js
@@ -1,34 +1,27 @@
-let galleries = document.querySelectorAll(".c-gallery");
+let galleries = document.querySelectorAll(".c-gallery")
 
-for (let i = 0; i < galleries.length; i++) {
-    let tileContainer = galleries[i].querySelector('.c-gallery__tiles')
+let paginateCards = (showCards, paginateCardAmount, tiles, totalTiles, button) => {
+    console.log(showCards, paginateCardAmount)
+    for (let x = showCards; x < (showCards + paginateCardAmount); x++)
+        tiles[x]?.classList.remove("c-gallery__tiles__tile--hidden")   
+
+    showCards += paginateCardAmount
+
+    if (totalTiles <= showCards)
+        button.style.display = "none"
+
+    return showCards
+}
+
+galleries.forEach(gallery => {
+    let tileContainer = gallery.querySelector('.c-gallery__tiles')
     let tiles = tileContainer.children;
     let totalTiles = tileContainer.getAttribute("total-tiles");
-    let button = galleries[i].querySelector('.c-gallery__button')
-    let paginateCardAmount = 3
-    let showCards = 3
+    let button = gallery.querySelector('.c-gallery__button')
+    let paginateCardAmount = screen.width >= 768 ? 6 : 3    
+    let showCards = paginateCards(0, paginateCardAmount, tiles, totalTiles, button)
 
-    if(screen.width >= 768){
-        paginateCardAmount = 6
-        showCards = 6
-
-        for (let x = 3; x < (showCards && totalTiles); x++) {
-            tiles[x].classList.remove("c-gallery__tiles__tile--hidden")
-        }
-    }
-
-    if (totalTiles <= showCards) {
-        button.style.display = "none"
-    }
-
-    galleries[i].querySelector('.c-button').addEventListener("click", function (e) {
-        for (let x = showCards; x < ((showCards + paginateCardAmount) && totalTiles); x++) {
-            tiles[x].classList.remove("c-gallery__tiles__tile--hidden")
-        }
-        showCards = showCards + paginateCardAmount
-
-        if (totalTiles <= showCards) {
-            button.style.display = "none"
-        }
+    gallery.querySelector('.c-button').addEventListener("click", e => {
+        showCards = paginateCards(showCards, paginateCardAmount, tiles, totalTiles, button)
     });
-}
+});
